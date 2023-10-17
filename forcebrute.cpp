d01a0e27d1d254a779a6cdde95635c6cfc5b31d9
Fer riao <12415135+ferneyriao@users.noreply.github.com>
2023-10-17 10:46:15 -0500

    Create forcebrute.cpp

---
+++ forcebrute.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+
+// Función para verificar si un número es primo
+bool esPrimo(int num) {
+    if (num < 2) return false;
+    for (int i = 2; i * i <= num; ++i) {
+        if (num % i == 0) return false;
+    }
+    return true;
+}
+
+// Función para calcular la suma de los dígitos de un número
+int sumaDigitos(int num) {
+    int suma = 0;
+    while (num > 0) {
+        suma += num % 10;
+        num /= 10;
+    }
+    return suma;
+}
+
+int calcularD(int n) {
+    std::vector<int> resultados;
+    int num = 1;
+
+    while (resultados.size() < n) {
+        if (esPrimo(sumaDigitos(num))) {
+            resultados.push_back(num);
+        }
+        num++;
+    }
+
+    return resultados.back();
+}
+
+int main() {
+    int n = 100000000; // n = 10^8
+
+    int resultado = calcularD(n);
+
+    std::cout << "D(" << n << ") = " << resultado << std::endl;
+
+    return 0;
+}
